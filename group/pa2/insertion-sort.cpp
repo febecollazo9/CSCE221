2926a3152bd174cf5c11722690eb75d223dc1346
Clayton Dittman <claywd1210@build.tamu.edu>
2015-09-23 14:31:56 -0500

    added group files

---
+++ group/pa2/insertion-sort.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : insertion-sort.cpp
+// Author      : Febe Collazo
+// Date        : Sept. 21, 2015
+// Copyright   : Lecture slides: Analysis of Algorithms
+// Description : Implementation of insertion sort in C++
+//============================================================================
+
+#include "sort.h"
+
+void
+InsertionSort::sort(int A[], int size)				// main entry point
+{
+	
+  /* Complete this function with the implementation of insertion sort algorithm 
+  Record number of comparisons in variable num_cmps of class Sort
+  */
+  for (int k = 1; k < size; k++)
+  {
+	int tmp = A[k];
+	int j = k;
+	num_cmps++;
+	for (; j > 0 && tmp < A[j-1]; j--){
+		if (j!=k) num_cmps++;
+		A[j] = A[j-1];
+	}
+	A[j] = tmp;
+  }
+}
